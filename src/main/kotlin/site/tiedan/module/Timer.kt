878f680cfe63dad904a45c9512d1aaf2b006c9f8
tiedanGH <2295824927@qq.com>
2025-12-18 20:54:30 -0500

    feat: implement automatic backup for YML data files

--- src/main/kotlin/site/tiedan/module/Timer.kt
+++ src/main/kotlin/site/tiedan/module/Timer.kt
@@ -1,14 +1,20 @@
 package site.tiedan.module
 
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import site.tiedan.MiraiCompilerFramework.logger
 import site.tiedan.MiraiCompilerFramework.save
 import site.tiedan.MiraiCompilerFramework.roundTo2
 import site.tiedan.data.ExtraData
 import net.mamoe.mirai.utils.info
+import site.tiedan.MiraiCompilerFramework
+import java.io.File
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 object Timer {
     fun executeScheduledTasks() {
+        backupYmlDataFiles()
         executeClearBlackList()
         dailyDecayScore()
     }
@@ -41,6 +47,36 @@ object Timer {
         }
         ExtraData.save()
         logger.info { "热度指数衰减执行完成" }
+    }
+
+    private fun backupYmlDataFiles() {
+        @OptIn(ConsoleExperimentalApi::class)
+        val baseDir = File("./data/${MiraiCompilerFramework.dataHolderName}/")
+        if (!baseDir.exists() || !baseDir.isDirectory) return
+
+        val bakDir = File(baseDir, "backup")
+        if (!bakDir.exists()) bakDir.mkdirs()
+
+        val today = LocalDate.now().format(DateTimeFormatter.ISO_DATE)
+        val todayDir = File(bakDir, today)
+        if (!todayDir.exists()) todayDir.mkdirs()
+
+        baseDir.listFiles { file ->
+            file.isFile && file.extension.equals("yml", ignoreCase = true)
+        }?.forEach { ymlFile ->
+            val target = File(todayDir, ymlFile.name)
+            ymlFile.copyTo(target, overwrite = true)
+        }
+
+        val dateDirs = bakDir.listFiles { file -> file.isDirectory }?.toList() ?: return
+        if (dateDirs.size > 7) {
+            val sorted = dateDirs.sortedBy { it.name }
+            val toDelete = sorted.take(dateDirs.size - 7)
+            toDelete.forEach { dir ->
+                dir.deleteRecursively()
+            }
+        }
+        logger.info { "YML 数据文件自动备份完成" }
     }
 
     private fun getCalender(hour: Int, minute: Int = 0, second: Int = 0, millisecond: Int = 0): Calendar {
