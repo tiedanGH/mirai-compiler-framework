df3ade86524ae7bd0f7c46479ae32c3ccc5757a8
tiedanGH <2295824927@qq.com>
2026-09-09 23:38:07 -0400

    chore(release): set version to 2.1.0

--- src/main/kotlin/site/tiedan/MiraiCompilerFramework.kt
+++ src/main/kotlin/site/tiedan/MiraiCompilerFramework.kt
@@ -38,7 +38,7 @@ object MiraiCompilerFramework : KotlinPlugin(
     JvmPluginDescription(
         id = "site.tiedan.mirai-compiler-framework",
         name = "Mirai Compiler Framework",
-        version = "2.0.0",
+        version = "2.1.0",
     ) {
         author("tiedan")
         info("""基于Glot接口的在线编译器框架""")
